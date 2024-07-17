ea002b3451eeb56ba9815e2f7a3d18d256230b96
Shivam Singh <Shivamsingh2211653@gmail.com>
2024-07-17 23:48:34 +0530

    added constant package and Constants class

---
+++ src/main/java/com/example/SpringBoot_CRUDOperationsAPI/constant/Constants.java
@@ -0,0 +1,8 @@
+package com.example.SpringBoot_CRUDOperationsAPI.constant;
+
+public class Constants {
+
+    public static final String DATABASE_URL ="jdbc:mysql://localhost:3306/company_db";
+    public static final String DATABASE_NAME ="root";
+    public static final String DATABASE_PASSWORD ="root1";
+}
