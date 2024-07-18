4000ca24bd1023127b69d98d863f6b5b7a5fe8d7
ankit kumar ahirwar <ahirwarankit63@gmail.com>
2024-07-18 08:37:47 +0530

    created empRepo pakage and interface

---
+++ src/main/java/com/example/SpringBoot_CRUDOperationsAPI/repository/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.example.SpringBoot_CRUDOperationsAPI.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository {
+}
--- src/main/java/com/example/SpringBoot_CRUDOperationsAPI/serviceimpl/ServiceImpl.java
+++ src/main/java/com/example/SpringBoot_CRUDOperationsAPI/serviceimpl/ServiceImpl.java
@@ -1,5 +1,11 @@
 package com.example.SpringBoot_CRUDOperationsAPI.serviceimpl;
 
-public class ServiceImpl {
-    //
+import com.example.SpringBoot_CRUDOperationsAPI.repository.EmployeeRepository;
+import com.example.SpringBoot_CRUDOperationsAPI.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ServiceImpl implements EmployeeService{
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
 }
